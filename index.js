555034de22c6560b5c3ca562dbfe45286106aa29
astounding-spooling-2354 <nlashner@gmail.com>
2020-01-14 23:04:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 
 function shout(string){
-  let newStr = ''
-  for (let i = 0; i < string.length; i++){
-    letter = i.toUpperCase()
-    newStr += letter
-  }
-  return newStr
+  return string.toUpperCase()
 }
